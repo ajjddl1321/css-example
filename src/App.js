ef46d51b5ea40ed13a982967322ad7496808edb4
ajjddl1321 <tjrgh1321@naver.com>
2023-03-28 16:21:54 +0900

    추가

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button1 from "./css/Button1"
+import Button2 from "./css/Button2"
+import Tailwindcomponent from "./css/Tailwindcomponent";
+
+export default function App()
+{
+    return(
+        <>
+        <Button1/>
+        <Button2/>
+        <Tailwindcomponent/>
+        </>
+    )
+}
+
---
+++ src/css/Tailwindcomponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Tailwindcomponent()
+{
+    return(
+        <div>
+            <h1 className="text-9xl">안녕</h1>
+            <button className="bg-blue-500 rounded-xl px-2">버튼</button>
+        </div>
+    )
+}
