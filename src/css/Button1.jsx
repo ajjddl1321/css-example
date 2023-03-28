ef7c2cce4b89f7031fdc4a4058e7673cf4e71c3d
ajjddl1321 <tjrgh1321@naver.com>
2023-03-28 16:42:44 +0900

    .

--- src/css/Button1.jsx
+++ src/css/Button1.jsx
@@ -3,5 +3,5 @@ import styles from './Button1.module.css';
 
 export default function Button1()
 {
-    return <button className={styles.button}>버튼1</button>;
+    return <button className={styles.button}>버튼 수정</button>;
 }
